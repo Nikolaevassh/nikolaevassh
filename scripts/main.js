d007918949c442bfc7be28251abe11ef083e5172
nikolaevassh <snickolaevaa@gmail.com>
2018-01-04 22:02:27 +0300

    adding my files to my repository

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var myScr = myImage.getAttribute('src');
+    if(myScr === 'images/poljot-pchely-vokrug-granata.jpg') {
+        myImage.setAttribute ('src','images/moja-zhena.jpg');
+    } else {
+        myImage.setAttribute ('src','images/poljot-pchely-vokrug-granata.jpg');
+    }
+}
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+function setUserName() {
+    var myName = prompt('Please,enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+    }
+    if(!localStorage.getItem('name')) {
+        setUserName();
+        } else {
+            var storedName = localStorage.getItem('name');
+            myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+            }
+myButton.onclick = function() {
+    setUserName();
+}
